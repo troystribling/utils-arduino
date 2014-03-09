5c5fd402f8a05c5b2df5bbede8ecf5e35dbd564e
Troy Stribling <troy.stribling@gmail.com>
2014-03-09 12:01:50 -0700

    added free memory macro

--- log.h
+++ log.h
@@ -1,10 +1,14 @@
 #ifndef LOG_H
 #define LOG_H
 
+#include "memory_free.h"
+
 #ifdef DBUG
   #define DBUG_LOG(...) Serial.print(F("DBUG: ")); Serial.println(__VA_ARGS__)
+  #define DBUG_FREE_MEMORY Serial.print(F("DBUG: Free Memory: "));Serial.print(freeMemory());Serial.println(F(" bytes"))
 #else
   #define DBUG_LOG(...)
+  #define DBUG_FREE_MEMORY
 #endif
 
 #ifdef INFO
