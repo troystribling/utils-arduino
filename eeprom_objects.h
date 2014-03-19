50a17c8e0afe4b404ee91ecf29435985a3166cd9
Troy Stribling <troy.stribling@gmail.com>
2014-03-19 08:28:57 -0700

    new thoughts on interface

--- eeprom_objects.h
+++ eeprom_objects.h
@@ -4,21 +4,24 @@
 template <class T> class EEPROMObject {
 public:
     EEPROMObject(uint16_t _offset, uint8_t _maxObjects) : offset(_offset), maxObjects(_maxObjects){};
-    uint16_t create(const T& value);
-    uint16_t update(uint16_t index, const T& value);
-    uint16_t read(uint16_t index, T& value);
-    uint16_t remove(uint16_t index);
+    uint16_t create(uint8_t& index, const T& value);
+    uint16_t update(uint8_t index, const T& value);
+    uint16_t read(uint8_t index, T& value);
+    uint16_t remove(uint8_t index);
+    uint16_t next(uint8_t& index, T& value);
     uint8_t count();
 private:
-    uint16_t location(int16_t index, uint16_t size){return (offset + index * size);};
-    uint8_t nextLocation(uint16_t size);
+    uint16_t location(int8_t index, uint16_t size){return (offset + index * size);};
+    uint16_t nextFreeLocation(uint16_t size);
+    uint8_t nextUsedIndex(uint8_t currentIndex, uint16_t size);
     uint16_t offset;
     uint8_t  maxObjects;
 };
 
-template <class T> uint16_t EEPROMObject<T>::create(const T& value) {
+template <class T> uint16_t EEPROMObject<T>::create(uint8_t& index, const T& value) {
     const byte* v = (const byte*)(const void*)&value;
-    uint16_t loc = nextLocation(sizeof(value)), i;
+    uint16_t loc = nextFreeLocation(sizeof(value)), i;
+    index = (loc - offset) / sizeof(value);
     for (i = 0; i < sizeof(value); i++) {
         EEPROM.write(loc, *v);
         loc++;
@@ -27,7 +30,7 @@ template <class T> uint16_t EEPROMObject<T>::create(const T& value) {
     return i;
 }
 
-template <class T> uint16_t EEPROMObject<T>::update(uint16_t index, const T& value) {
+template <class T> uint16_t EEPROMObject<T>::update(uint8_t index, const T& value) {
     const byte* v = (const byte*)(const void*)&value;
     uint16_t loc = location(index, sizeof(value)), i;
     for (i = 0; i < sizeof(value); i++) {
@@ -38,7 +41,7 @@ template <class T> uint16_t EEPROMObject<T>::update(uint16_t index, const T& val
     return i;
 }
 
-template <class T> uint16_t EEPROMObject<T>::read(uint16_t index, T& value) {
+template <class T> uint16_t EEPROMObject<T>::read(uint8_t index, T& value) {
     byte* v = (byte*)(void*)&value;
     uint16_t loc = location(index, sizeof(value)), i;
     for (i = 0; i < sizeof(value); i++) {
@@ -49,27 +52,46 @@ template <class T> uint16_t EEPROMObject<T>::read(uint16_t index, T& value) {
     return i;
 }
 
-template <class T> uint16_t EEPROMObject<T>::remove(uint16_t index) {
+template <class T> uint16_t EEPROMObject<T>::next(uint8_t& nextIndex, T& value) {
+    nextIndex = nextUsedIndex(nextIndex, sizeof(value));
+    return read(nextIndex, value);
+}
+
+template <class T> uint16_t EEPROMObject<T>::remove(uint8_t index) {
     uint16_t loc = location(index, sizeof(T));
     EEPROM.write(loc, 0x0);
 }
 
 template <class T> uint8_t EEPROMObject<T>::count() {
     uint8_t cnt = 0;
-    for (int i = 0; i < maxObjects; i++) {
-        uint8_t loc = location(i, sizeof(T));
+    uint8_t loc = location(0, sizeof(T));
+    for (uint8_t i = 0; i < maxObjects; i++) {
         if (EEPROM.read(loc) != 0x0) {
             cnt++;
         }
+        loc += sizeof(T);
     }
     return cnt;
 }
 
-template <class T> uint8_t EEPROMObject<T>::nextLocation(uint16_t size) {
+// private
+template <class T> uint16_t EEPROMObject<T>::nextFreeLocation(uint16_t size) {
     uint16_t loc = offset;
     uint8_t i;
     for (i = 0; i < maxObjects; i++) {
         if (EEPROM.read(loc) == 0x0) {
+            break;
+        }
+        loc += size;
+    }
+    return i;
+}
+
+template <class T> uint8_t EEPROMObject<T>::nextUsedIndex(uint8_t currentIndex, uint16_t size) {
+    uint16_t loc = location(currentIndex, size);
+    uint8_t i;
+    for (i = currentIndex; i < maxObjects; i++) {
+        if (EEPROM.read(loc) != 0x0) {
             break;
         }
         loc += size;
